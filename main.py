422a2d2ce96c0235faebeaf8c0ea904a2b4c5dae
nirmal-dentsu <nirmal.rajpurohit@dentsu.com>
2026-04-03 12:42:06 +0530

    modified main

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ mcp=FastMCP(name="Expense Tracker")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(
-    DATABASE_URL,
-    sslmode="require"
-)
+print("DATABASE_URL:", DATABASE_URL)   # 👈 add this
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 print("Connected successfully!")
 
 
